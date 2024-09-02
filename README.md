89761bd313bd2d635feb4ab45ed49d561998fd8b
titi-rex <reina@pearl.here>
2024-09-02 18:37:38 +0200

    ADD: readme + valgrind

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
-# libasm_tester_leadwasp
-LeadWasp, a tester for the 42 project libasm 
 
-# Install 
- 
- `git clone git@github.com:titi-rex/leadwasp_libasm_tester.git leadwasp`
- `cd leadwasp`
- `alias lw="$(pwd)/script/leadwasp.sh -p $(pwd) -l \"\\\$(pwd)\""`
- `alias leadwasp="$(pwd)/script/leadwasp.sh -p $(pwd)"`
- `>> ~/.zshrc echo "alias lw=\"$(pwd)/script/leadwasp.sh -p $(pwd) -l \\\$(pwd)\""`
- `>> ~/.zshrc echo "alias leadwasp=\"$(pwd)/script/leadwasp.sh -p $(pwd)\""`
+# LeadWasp
 
+A libasm tester for 42 student struggling to find asm lover to help them
+
+
+## Installation
+
+```bash
+  
+ git clone git@github.com:titi-rex/leadwasp_libasm_tester.git leadwasp
+ cd leadwasp
+ alias lw="$(pwd)/script/leadwasp.sh -p $(pwd) -l \"\\\$(pwd)\""
+ alias leadwasp="$(pwd)/script/leadwasp.sh -p $(pwd)"
+ >> ~/.zshrc echo "alias lw=\"$(pwd)/script/leadwasp.sh -p $(pwd) -l \\\$(pwd)\""
+ >> ~/.zshrc echo "alias leadwasp=\"$(pwd)/script/leadwasp.sh -p $(pwd)\""
+
+```
+You can clone the repo where you want, just make sure to set the good path in alias (*should be automatic if you follow Installation*)
+## Usage
+You are provided with two alias :  use `lw` inside your project repo or `leadwasp` with `-l path/to/libasm.a` 
+
+You can ask for `all`/`a``bonus`/`b` `mandatory`/`m` or specific function : 
+```bash
+    lw mandatory
+    lw -v a
+    lw -v strlen strcpy list_size
+    lw bonus
+    leadwasp -l /home/ellanae/libasm -n libasm.v2.a
+
+```
+
+Flags :
+- `-p` specify tester location (*default: automatically set in alias* `lw/leadwasp`) 
+- `-l` specify library location (*default:* `../`)
+- `-n` specify library name (*default:* `libasm.a`)
+- `-k` launch test with valgrind
+- `-v` verbose
+- `-h` show help
+
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
 
--- script/leadwasp.sh
+++ script/leadwasp.sh
@@ -14,14 +14,22 @@ function test() {
 }
 
 function help () {
-    echo "help"
+    echo "#==# LeadWasp, a libasm tester for 42 student #==#
+* use with 'lw' or 'leadwasp' alias
+* options : 
+*    -p specify tester location (*default: automatically set in alias* lw/leadwasp) 
+*    -l specify library location (*default:* ../)
+*    -n specify library name (*default:* libasm.a)
+*    -k launch test with valgrind
+*    -v verbose
+*    -h show help"
     exit 1;
 }
 
 MAKE="make -s -C" 
 
 TEST_PATH=$(pwd)
-LIB_PATH="../libasm"
+LIB_PATH="../"
 LIB_NAME="libasm.a"
 
 LIST_MANDATORY="strlen strcpy strcmp strdup read write"
@@ -29,25 +37,31 @@ LIST_BONUS="atoi_base list_push_front list_size list_sort list_remove_if"
 DONE=0
 
 
-while getopts "vhp:l:" arg; do
+while getopts "vhkp:l:" arg; do
     case $arg in 
-        h)  help ;;
         p)  TEST_PATH="${OPTARG}";;
         l)  LIB_PATH="${OPTARG}";;
         n)  LIB_NAME="${OPTARG}";;
         v)  VERBOSE="verbose";;
-        *)  echo "$i not supported"; exit 1;;
+        k)
+            if [ $(>/dev/null 2>&1 valgrind --version && echo $?) -eq 0 ]; then
+                VALGRIND="valgrind --quiet --leak-check=full --errors-for-leak-kinds=all --error-exitcode=1 ";
+            else
+                echo "valgrind not installed!";
+            fi
+        ;;
+        h | *)  help ;;
     esac
 done
 
 
 for i in "$@"; do
     case $i in 
-        a)  DONE=1; maker $i && test "$LIST_MANDATORY" && test "$LIST_BONUS" ;;
-        m)  DONE=1; maker $i && test "$LIST_MANDATORY" ;;
-        b)  DONE=1; maker $i && test "$LIST_BONUS" ;;
+        a | all)  DONE=1; maker $i && test "$LIST_MANDATORY" && test "$LIST_BONUS" ;;
+        m | mandatory)  DONE=1; maker $i && test "$LIST_MANDATORY" ;;
+        b | bonus)  DONE=1; maker $i && test "$LIST_BONUS" ;;
         strlen | strcpy | strcmp | strdup | read | write | atoi_base | list_size | list_push_front | list_sort | list_remove_if) 
-            DONE=1; maker "bin/$i.out" && $TEST_PATH/bin/$i.out $VERBOSE;;
+            DONE=1; maker "bin/$i.out" && $VALGRIND $TEST_PATH/bin/$i.out $VERBOSE ;;
         clear) DONE=1; maker fclean ;;
     esac
 done
