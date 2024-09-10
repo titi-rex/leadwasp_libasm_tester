06932216195c9b77367d52aa448f2518c96d3cbc
reina <13titirex@pm.me>
2024-09-10 18:32:11 +0200

    Squashed 'external/libunit/' content from commit 550be77
    git-subtree-dir: external/libunit
    git-subtree-split: 550be7716f2f9427fc1ba21e5f57a32bdf1aefdb

---
+++ libunit/inc/libunit.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libunit.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 20:55:43 by tlegrand          #+#    #+#             */
+/*   Updated: 2024/09/08 14:40:48 by tlegrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBUNIT_H
+# define LIBUNIT_H
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdint.h>
+
+# define NAME_MAX_LENGHT 32
+# define TIMEOUT 2
+# define TIMEOUT_L 10
+# define LOG_BUFFER_SIZE 300
+
+# define INTERNAL_ERROR 1
+
+# define FLAG_STDIN		0b00000001
+# define FLAG_TIMEOUT_L	0b00000010
+
+typedef int		(*t_launcher_func)(void);
+typedef int		(*t_test_func)(void);
+typedef void	(*t_verbose)(void);
+
+typedef struct s_test
+{
+	char			name[NAME_MAX_LENGHT];
+	size_t			id;
+	uint8_t			flags;
+	uint8_t			status;
+	t_test_func		function;
+	t_verbose		scenario;
+	struct s_test	*next;
+}					t_test;
+
+void	test_loader(t_test **test_list, char *test_name, t_test_func function, uint8_t flags);
+int		test_launcher(t_test **test_list, const char *name);
+
+#endif
---
+++ tests-example/inc/tests.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 22:41:50 by tlegrand          #+#    #+#             */
+/*   Updated: 2024/09/08 13:45:01 by tlegrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTS_H
+# define TESTS_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+int	strlen_launcher(void);
+int	ok_short(void);
+int	ok_long(void);
+int	ko_bad_result(void);
+int	ko_segv(void);
+int	ko_buse(void);
+int	ko_timeout(void);
+
+int	write_launcher(void);
+int	ok_short_string(void);
+int	ok_short_string_ret(void);
+int	ok_empty(void);
+int	ko_bad_fd(void);
+
+
+#endif
